55e27d96bae7bfe0012045b1b39db8512051e2f7
Ahmed Ragy <153857858+Ahmed-Ragy-3@users.noreply.github.com>
2026-07-21 13:03:01 +0300

    Revert "Model"

--- backend/FileUpload/IFileUploadService.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace backend.FileUpload {
-    public interface IFileUploadService {
-        Task<string> UploadAsync(IFormFile file);
-    }
-}
--- backend/Options/CloudinaryOptions.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace backend.Options {
-    public class CloudinaryOptions {
-        public string NAME { get; set; } = string.Empty;
-        public string API_KEY { get; set; } = string.Empty;
-        public string API_SECRET { get; set; } = string.Empty;
-    }
-
-}
