beb18f3845f4b2e5a31991b22702ce20c9d17174
AhmedAlaa8100 <ahmedalaa8100@gmail.com>
2026-07-29 18:02:53 +0300

    added get and update profile info for different roles

--- backend/Program.cs
+++ backend/Program.cs
@@ -55,7 +55,8 @@ builder.Services.AddDbContext<AppDbContext>(cfg => cfg.UseSqlServer(
 builder.Services.AddScoped<IFreelancerRepository, FreelancerRepository>();
 builder.Services.AddScoped<IFreelancerDashboardRepository, FreelancerDashboardRepository>();
 builder.Services.AddScoped<IFreelancerApplicationRepository, FreelancerApplicationRepository>();
-
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IClientRepository, ClientRepository>();
 // ===== Auth setup =====
 builder.Services.AddScoped<JwtService>();
 
@@ -125,14 +126,14 @@ static class DependencyInjection {
     public static IServiceCollection AddServices(this IServiceCollection services) {
         services.AddScoped<BookmarkService>();
         services.AddScoped<CategoryService>();
-        services.AddScoped<ClientService>();
-        services.AddScoped<FreelancerService>();
         services.AddScoped<HomeService>();
         services.AddScoped<JobService>();
 
+        services.AddScoped<IClientService, ClientService>();
         services.AddScoped<IFreelancerService, FreelancerService>();
         services.AddScoped<IFreelancerDashboardService, FreelancerDashboardService>();
         services.AddScoped<IFreelancerApplicationService, FreelancerApplicationService>();
+        services.AddScoped<IUserService, UserService>();
 
         return services;
     }
